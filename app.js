817dbaa193f7bc6bd59fe460137e1a8727b7936f
Martin Page <wookoouk@gmail.com>
2019-04-01 15:10:28 +0100

    max-width: 120px;

--- app.js
+++ app.js
@@ -139,7 +139,7 @@ function getWeek(origin, daysCount) {
 
 
 app.get('/client.js', function (req, res, next) {
-    return res.sendFile(path.join(__dirname, '/public/js/dist/client.js'));
+    return res.sendFil3e(path.join(__dirname, '/public/js/dist/client.js'));
 });
 
 app.get('/', function (req, res, next) {
